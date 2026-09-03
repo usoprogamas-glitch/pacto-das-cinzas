66c01b17f37a3a50eb4d259f5ac988b4d8574908
Ciell-VozdoMundo <usoprogamas-glitch@users.noreply.github.com>
2026-09-03 14:16:09 -0300

    chore: remove lixo temporário da investigação itch.io (htmls/zips/cookies) e ignora artefatos transitórios

--- tools/_extract_link2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-data = open("tools/overworld.zip", "rb").read().decode("utf-8", errors="replace")
-print("tamanho html:", len(data))
-# Procura qualquer URL com /file/ ou .zip
-for pattern in [r'(https?://[^"\']*\.zip[^"\']*)', r'(?:"|\')([^"\']*\/file\/[^"\']*)', r'window\.location[^;]*']:
-    m = re.findall(pattern, data)
-    if m:
-        print("ACHOU", pattern[:20], ":", m[:3])
